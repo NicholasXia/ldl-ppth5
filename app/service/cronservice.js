951f23e84cd1cc6111079c15b83fd0c4b74604ed
xiayuanfeng <xiayuanfeng@yapot.cn>
2016-04-04 23:51:00 +0800

    增加定时器

--- app/service/cronservice.js
+++ app/service/cronservice.js
@@ -1,6 +1,6 @@
 var cron=function(){
   var spawn = require('child_process').spawn;
-  var curl = spawn('curl', ['-G', 'http://localhost:3000/test_check']);
+  var curl = spawn('curl', ['-G', 'http://localhost:3000/api/test_check']);
 
   curl.on('close', (code) => {
     console.log(`child process exited with code ${code}`);
