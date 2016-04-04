ef8eab1ca533bf6935406df093da26549df7a558
xiayuanfeng <xiayuanfeng@yapot.cn>
2016-04-04 23:48:34 +0800

    2秒执行一次压缩

--- app/service/cronservice.js
+++ app/service/cronservice.js
@@ -1,6 +1,10 @@
-var spawn = require('child_process').spawn;
-var curl = spawn('curl', ['-G', 'http://localhost:3000/test_check']);
+var cron=function(){
+  var spawn = require('child_process').spawn;
+  var curl = spawn('curl', ['-G', 'http://localhost:3000/test_check']);
 
-curl.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
+  curl.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+}
+
+setInterval(cron,2000);
