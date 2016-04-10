8b73d0a30862be023c8e97f744c7bc64d4a694a0
xiayuanfeng <xiayuanfeng@yapot.cn>
2016-04-10 21:36:29 +0800

    修改html内容

--- app/service/h5service.js
+++ app/service/h5service.js
@@ -8,6 +8,16 @@ var config = require('../../config/config');
 var fsExtra = require('fs-extra');
 var path = config.upload.path;
 var h5path = config.upload.h5path;
+
+var renderHTML = function(src, dest, cb) {
+  fs.readFile(src, function(err, data) {
+    var str = data + "";
+    var updateStr = str.replace('</head>', '<script src="ldlh5.js" charset="utf-8"></script></head>').replace('start();', 'start();ldlinit();');
+    fs.writeFile(dest, updateStr, cb);
+  });
+}
+
+
 var check = function(cb) {
   var finalPath = __dirname.replace('app/service', "") + path;
   uploadFileModel.find({
@@ -32,45 +42,54 @@ var check = function(cb) {
           //Copy js
           console.log('src ', global.PROJECT_URI + '/public/js/ldlh5.js');
           console.log('dest ', newDirPath + "/ldlh5.js");
+
+
           fsExtra.copy(global.PROJECT_URI + '/public/js/ldlh5.js', newDirPath + "/ldlh5.js", function(err) {
             if (err) return console.log('copy js ', err)
             console.log("copy ldlh5 success!");
-            //压缩
-            var read = targz().createReadStream(newDirPath);
-            console.log('tar.gz=', h5path + f.name + '.tar.gz')
-            var write = fs.createWriteStream(h5path + f.name + '.tar.gz');
-            read.pipe(write);
-            var zipName = f.name + '.tar.gz';
-            //POST回调地址
-            var callback_url = f.callback_url;
-            var form = {
-              name: f.name,
-              ext: f.ext,
-              date: f.date,
-              path_url: 'http://123.56.184.87:8000/' + zipName
-            }
-            //调用回调
-            request.post({
-              url: callback_url,
-              form: form
-            }, function(err, httpResponse, body) {
-              console.log('post 回调 ', body);
-            })
+            //覆盖HTML
+            renderHTML(newDirPath + "/index.html", newDirPath + "/index.html", function(err) {
+              if (err) console.log('重写文件失败 ', err);
+              console.log('重写文件成功')
 
-            //更新状态
-            uploadFileModel.update({
-              _id: f.id
-            }, {
-              $set: {
-                status: 1
-              }
-            }, function(err, num) {});
+              //压缩
+              var read = targz().createReadStream(newDirPath);
+              console.log('tar.gz=', h5path + f.name + '.tar.gz')
+              var write = fs.createWriteStream(h5path + f.name + '.tar.gz');
+              read.pipe(write);
+              var zipName = f.name + '.tar.gz';
+              //POST回调地址
+              var callback_url = f.callback_url;
+              var form = {
+                  name: f.name,
+                  ext: f.ext,
+                  date: f.date,
+                  path_url: 'http://123.56.184.87:8000/' + zipName
+                }
+                //调用回调
+              request.post({
+                url: callback_url,
+                form: form
+              }, function(err, httpResponse, body) {
+                console.log('post 回调 ', body);
+              })
+
+              //更新状态
+              uploadFileModel.update({
+                _id: f.id
+              }, {
+                $set: {
+                  status: 1
+                }
+              }, function(err, num) {});
+
+            });
+
+
+
 
 
           });
-
-
-
 
         } else {
           console.log(f.name + ' 没有完成');
@@ -80,5 +99,4 @@ var check = function(cb) {
     cb();
   });
 }
-
 exports.check = check;
