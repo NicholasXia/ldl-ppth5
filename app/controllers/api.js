146aef8ba7db370e861121685ad2fb45c0d4b1ce
xiayuanfeng <xiayuanfeng@yapot.cn>
2016-04-13 17:42:52 +0800

    修改接口

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -111,14 +111,16 @@ router.get('/test_check',function(req,res,next){
 
 router.get('/list',function(req,res,next){
   var display={
+    _id:0,
+    id:0,
     name: 0,
     ext:0,
     path:0,
     status:0,
     callback_url:0,
-    date: Date
+    date: 0
   }
-  uploadFileModel.find(display,{status:1}).sort({date:-1}).exec(function(err,files){
+  uploadFileModel.find({status:1},display).sort({date:-1}).exec(function(err,files){
     res.json(files);
   });
 });
